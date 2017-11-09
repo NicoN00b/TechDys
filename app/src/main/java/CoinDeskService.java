e462ff0e4113d0efd2e2057c42a819db9ebe3e05
nr <nicholas.raethke@gmail.com>
2017-11-08 17:39:18 -0800

    set up project

---
+++ app/src/main/java/CoinDeskService.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Guest on 11/8/17.
+ */
+
+public class CoinDeskService {
+
+    public static void findBitIndex
+}
---
+++ app/src/main/java/techdys/grailsoft/com/thetechdystopiaproject/Constants.java
@@ -0,0 +1,14 @@
+package techdys.grailsoft.com.thetechdystopiaproject;
+
+
+public class Constants {
+
+    public static final String COINDESK_BASE_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    public static final String COINDESK_USD = "USD";
+    public static final String COINDESK_USD = "USD";
+    public static final String COINDESK_USD = "USD";
+
+
+}
+
+//Retrofit Api Check it out
