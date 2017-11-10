d1c777c5378cc2ef949f3a2dbe18aec4300d61d7
nr <nicholas.raethke@gmail.com>
2017-11-09 16:41:46 -0800

    set up coindesk files

--- app/src/main/java/techdys/grailsoft/com/thetechdystopiaproject/CoinDeskService.java
+++ app/src/main/java/techdys/grailsoft/com/thetechdystopiaproject/CoinDeskService.java
@@ -1,3 +1,5 @@
+package techdys.grailsoft.com.thetechdystopiaproject;
+
 /**
  * Created by Guest on 11/8/17.
  */
---
+++ app/src/main/java/techdys/grailsoft/com/thetechdystopiaproject/model/CoinDesk.java
@@ -0,0 +1,9 @@
+package techdys.grailsoft.com.thetechdystopiaproject.model;
+
+/**
+ * Created by Guest on 11/9/17.
+ */
+
+public class CoinDesk {
+
+}
